71d49b94a96ea89625caf56bbbba2783b471ad65
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-18 13:36:26 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,44 @@
-import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.sql.SQLException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
         Class<Sample> sampleClassObject = Sample.class;
 
-        //  https://docs.oracle.com/javase/7/docs/api/java/lang/Class.html#getDeclaredConstructors()
-        //  NB: This method returns an array of length 0 if this Class object represents an interface, a primitive type, an array class, or void
-        Constructor<?>[] allConstructors  = sampleClassObject.getDeclaredConstructors();
-        Constructor<?>[] publicConstructors  = sampleClassObject.getConstructors();
+        //  this method returns the public fields of the class
+        //  and of all its superclasses and superinterfaces.
+        Field[] publicFields = sampleClassObject.getFields();
 
-        for (Constructor<?> c: publicConstructors) {
-            System.out.println("Public Constructors: " + c);
+        for(Field f: publicFields) {
+            System.out.println("Public fields: " + f);
+            System.out.println("\t>> Modifier:\t" + Modifier.toString(f.getModifiers()));
         }
 
-        System.out.println("-------------------------------------");
+        System.out.println("--------------------------");
 
-        for (Constructor<?> c: allConstructors) {
-            System.out.println("All Constructors: " + c);
-        }
+        //  all the fields declared by the class or interface
+        //  represented by this Class object.
+        //  This includes public, protected, default (package) access,
+        //  and private fields, but excludes inherited fields.
+        Field[] allFields = sampleClassObject.getDeclaredFields();
 
-        System.out.println("-------------------------------------");
-
-        //  The elements in the array returned are not sorted and are not in any particular order
-        Class<?>[] parameterVector = publicConstructors[0].getParameterTypes();
-        Class<?>[] exceptionsVector = publicConstructors[0].getExceptionTypes();
-
-        for (int i = 0; i < parameterVector.length; ++i) {
-            System.out.println("Parameter at position: " + i + " >> " + parameterVector[i]);
-        }
-
-        System.out.println("-----------------------------");
-
-        for (int i = 0; i < exceptionsVector.length; ++i) {
-            System.out.println("Exception at position: " + i + " >> " + exceptionsVector[i]);
+        for(Field f: allFields) {
+            System.out.println("All fields: " + f);
+            System.out.println("\t>> Modifier:\t" + Modifier.toString(f.getModifiers()));
         }
     }
 }
 
-class Sample {
-    public Sample() {
-    }
+class Demo {
+    public double price = 0.0;
+    private int pages = 100;
+    protected String message = "Hi there!";
+}
 
-    private Sample(int i) {
-    }
-
-    protected Sample(double d) {
-
-    }
-
-    public Sample(int i, LocalDateTime ldt) throws IOException, NullPointerException {
-    }
-
-    public Sample(int i, LocalDateTime ldt, String message) throws IOException, NullPointerException {
-    }
-
-    public Sample(int i, LocalDateTime ldt, String message, String title) throws IOException, NullPointerException {
-    }
-
-    public Sample(int i, LocalDateTime ldt, String message, boolean flag) throws IOException, NullPointerException {
-    }
+class Sample extends Demo {
+    public LocalDateTime now = LocalDateTime.now();
+    private String sampleTitle = "Title";
+    protected String secret = "Universe";
+    public static final double PI = 3.14159265359;
 }
