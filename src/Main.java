3c844a227e6f5da211fc74027d726dbd710a309a
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-18 13:25:46 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //  Instance of Class<T> is the class or interface at runtime
+        //  This provides information about a given class or interface,
+        //  such as its name, superclass, interfaces, fields, and methods
+        Class<Integer> intClassObject = int.class;
+        Class<Integer> integerClassObject = Integer.TYPE;    //  of primitive
+        Class<Integer> integerWrapperClassObject = Integer.class;    //  of wrapper
+
+        System.out.println(intClassObject.getName());
+        System.out.println(integerClassObject.getName());
+        System.out.println(integerWrapperClassObject.getName());
+
+        Integer j = 100;
+        boolean instance = integerWrapperClassObject.isInstance(j);
+        System.out.println(instance);
     }
 }
