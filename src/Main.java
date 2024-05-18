e199cd4a3cfe119eebb213da52852ee165e8a87b
Vivekanand P V <vivekanandpv@outlook.com>
2024-05-18 13:31:45 +0530

    done

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,37 @@
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
         Class<Sample> sampleClassObject = Sample.class;
 
-        Method[] declaredMethods = sampleClassObject.getDeclaredMethods();
-        Method[] allMethods = sampleClassObject.getMethods();
+        Method method  = sampleClassObject.getDeclaredMethods()[0];
 
-        for (Method m: declaredMethods) {
-            System.out.println("Declared Methods: " + m.toString());
+        System.out.println("Method full name: " + method.toString());
+        System.out.println("Method name: " + method.getName());
+        System.out.println("Return type: " + method.getReturnType());
+
+        System.out.println("-----------------------------");
+
+        Class<?>[] parameterVector = method.getParameterTypes();
+        Class<?>[] exceptionsVector = method.getExceptionTypes();
+
+        for (int i = 0; i < parameterVector.length; ++i) {
+            System.out.println("Parameter at position: " + i + " >> " + parameterVector[i]);
         }
 
-        System.out.println("-------------------------------------");
+        System.out.println("-----------------------------");
 
-        for (Method m: allMethods) {
-            System.out.println("All Methods: " + m.toString());
+        for (int i = 0; i < exceptionsVector.length; ++i) {
+            System.out.println("Exception at position: " + i + " >> " + exceptionsVector[i]);
         }
     }
 }
 
 class Sample {
-    public void voidMethod(String message) {
-    }
-
-    public int intReturnMethod() {
-        return 100;
-    }
-
-    private void privateMethod() {
-    }
-
-    protected void protectedMethod() {
+    public LocalDateTime getLocalDate(String message, int number) throws IOException, NullPointerException, SQLException {
+        return LocalDateTime.now();
     }
 }
